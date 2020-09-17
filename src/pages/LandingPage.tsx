027c19bc14ae5b4fddfab846667102d4d64f6108
Tin Anh Nguyen <anhtin95@gmail.com>
2020-09-17 02:01:24 +0200

    feature(#8): add placeholder content

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,25 @@ import React from 'react';
 function LandingPage() {
   return (
     <>
-      <h1>Landing Page</h1>
+      <h1>Welcome to MAD Learning</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend et
+        ex id egestas. Fusce eu iaculis magna, eu auctor mauris. Mauris euismod
+        lacus sed tellus vulputate, ac ullamcorper sem efficitur. Suspendisse ut
+        posuere tortor. Morbi non imperdiet ipsum, vulputate imperdiet arcu.
+        Integer mi lorem, consectetur porta sem vel, pulvinar venenatis justo.
+        Nullam bibendum risus ac cursus viverra. Proin consequat nunc eros.
+      </p>
+      <p>
+        Nam gravida dui a nisl consequat, convallis luctus lacus euismod.
+        Pellentesque porttitor vulputate rutrum. Morbi aliquam orci sed mi
+        congue malesuada. Donec ac scelerisque risus, a tincidunt ex. Fusce
+        vestibulum vulputate consectetur. Etiam viverra, ex ut gravida rutrum,
+        turpis nulla congue elit, in congue mi nibh sed eros. In vestibulum,
+        turpis in hendrerit interdum, felis elit dictum urna, nec pharetra
+        mauris dolor quis justo. Suspendisse rhoncus tempus augue vel venenatis.
+        Mauris at ipsum vitae ex tincidunt imperdiet quis ut nulla.
+      </p>
     </>
   );
 }
