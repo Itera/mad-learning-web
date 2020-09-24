8ab04bc97197b86872c15c1b53b4277bc9fb0527
Martin Onstad <martin.onstad@itera.no>
2020-09-24 09:41:21 +0200

    fix #2: Setting date and time in right format. Event type must be filled.

--- src/components/CreateEvent/CreateEvent.tsx
+++ src/components/CreateEvent/CreateEvent.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { CreateEventWrapper, Label, Grid } from './styled';
+import { CreateEventWrapper, Form } from './styled';
+import { parseISO, formatISO, format } from 'date-fns';
+
 
 function CreateEvent() {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [imageAlt, setImageAlt] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("16:00");
+  // const [date, setDate] = useState(Date.now());
+  const [time, setTime] = useState(formatISO(Date.now()));
   const [description, setDescription] = useState("");
   const [eventType, setEventType] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -23,7 +25,7 @@ function CreateEvent() {
     },
       body: JSON.stringify({
         id: "5f6b241978964f06faa3185b",
-        time: "2020-09-23T10:35:08.398Z",
+        time: time,
         name: name,
         description: description,
         owner: {
@@ -47,7 +49,7 @@ function CreateEvent() {
   }
 
   function isFormFilled () {
-    if (name !== "" && description !== "") {
+    if (name !== "" && description !== "" && eventType !== "") {
       return true
     } else {
       return false
@@ -57,44 +59,59 @@ function CreateEvent() {
   return (
     <CreateEventWrapper>
       <h3>Create new event</h3>
-      <Grid>
-        <div><Label htmlFor="name">Event name:</Label></div>
-        <div><input name="name" type="text" value={name} onChange={(event) => setName(event.target.value)}/></div>
+      <Form>
+        <label htmlFor="name">
+          Event name:
+          <input name="name" type="text" value={name} onChange={(event) => setName(event.target.value)}/>
+        </label>
 
-        <div><Label htmlFor="time">Date and time:</Label></div>
-        <div>
-          <div><input name="date" type="date" value={date} onChange={(event) => setDate(event.target.value)}/></div>
-          <input name="time" type="time" value={time} onChange={(event) => setTime(event.target.value)}/>
-        </div>
-        
-        <div><Label htmlFor="description">Description:</Label></div>
-        <div><textarea name="description" value={description} onChange={(event) => setDescription(event.target.value)}></textarea></div>
+        <label htmlFor="time">
+          Date and time:
+          <input name="time" type="date" value={format(parseISO(time), 'yyyy-MM-dd')} onChange={(event) => setTime(formatISO(Date.parse(event.target.value + ' ' + format(parseISO(time), 'HH:mm'))))}/>
+          <input name="time" type="time" value={format(parseISO(time), 'HH:mm')} onChange={(event) => setTime(formatISO(Date.parse(format(parseISO(time), 'yyyy-MM-dd') + ' ' + event.target.value)))}/>
+        </label>
+
+        <label htmlFor="description">
+          Description:
+          <textarea name="description" value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
+        </label>
       
-        <div><Label htmlFor="image">Image link:</Label></div>
-        <div><input name="image" type="text" value={image} onChange={(event) => setImage(event.target.value)}/></div>
+        <label htmlFor="image">
+          Image link:
+          <input name="image" type="text" value={image} onChange={(event) => setImage(event.target.value)}/>
+        </label>
 
-        <div><Label htmlFor="imageAlt">Image text:</Label></div>
-        <div><input name="imageAlt" type="text" value={imageAlt} onChange={(event) => setImageAlt(event.target.value)}/></div>
+        <label htmlFor="imageAlt">
+          Image text:
+          <input name="imageAlt" type="text" value={imageAlt} onChange={(event) => setImageAlt(event.target.value)}/>
+        </label>
 
-        <div><Label htmlFor="eventType">Event type:</Label></div>
-        <div>
+        <label htmlFor="eventType">
+          Event type:
           <select name="eventType" value={eventType} onChange={(event) => setEventType(event.target.value)}>
+            <option value="" disabled={eventType !== ""}>Choose event type</option>
             <option value="Subject matter event">Subject matter event</option>
             <option value="Coding event">Coding event</option>
             <option value="Project presentation">Project presentation</option>
             <option value="Workshop">Workshop</option>
           </select>
-        </div>
+        </label>
 
-        <div><Label htmlFor="firstName">Owner first name:</Label></div>
-        <div><input name="firstName" type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)}/></div>
+        <label htmlFor="firstName">
+          Owner first name:
+          <input name="firstName" type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)}/>
+        </label>
 
-        <div><Label htmlFor="lastName">Owner last name:</Label></div>
-        <div><input name="lastName" type="text" value={lastName} onChange={(event) => setLastName(event.target.value)}/></div>
+        <label htmlFor="lastName">
+          Owner last name:
+          <input name="lastName" type="text" value={lastName} onChange={(event) => setLastName(event.target.value)}/>
+        </label>
 
-        <div><Label htmlFor="email">Owner email:</Label></div>
-        <div><input name="email" type="text" value={email} onChange={(event) => setEmail(event.target.value)}/></div>
-      </Grid>
+        <label htmlFor="email">
+          Owner email:
+          <input name="email" type="text" value={email} onChange={(event) => setEmail(event.target.value)}/>
+        </label>
+      </Form>
       <div><button disabled={!isFormFilled()} onClick={create}>Create</button></div>
       
     </CreateEventWrapper>
--- src/components/CreateEvent/styled.ts
+++ src/components/CreateEvent/styled.ts
@@ -2,27 +2,13 @@ import styled from 'styled-components';
 
 export const CreateEventWrapper = styled.div`
   margin: 0 2.5vw;
-  text-align: center;
 `;
 
-export const Label = styled.label`
-  display: block;
-`;
-
-export const Grid = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
-  grid-template-rows: auto;
-  justify-content: center;
-  column-gap: 10px;
-  row-gap: 15px;
+export const Form = styled.form`
   margin-bottom: 20px;
+  font-weight: bold;
 
   input, textarea, select {
-    width: 250px;
-  }
-
-  textarea {
-    height: 50px;
+    display: block;
   }
 `;
