35075abd594849785bd6cb2db8d540cb471e945a
Tin Anh Nguyen <anhtin95@gmail.com>
2021-01-20 22:17:58 +0100

    refactor: change refresh logic on RSVP
    Added the `useRefresh` hook which combined with `useAsync` can be used
    to force React to re-render or retry/redo arbitrary operations.

--- src/pages/EventPage/EventPage.tsx
+++ src/pages/EventPage/EventPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { RouteComponentProps } from '@reach/router';
 
 import FailedFetchAlert from 'src/components/FailedFetchAlert';
@@ -20,25 +20,14 @@ type EventPageProps = {
 function EventPage({ eventId, navigate, ...rest }: EventPageProps) {
   const tmpEventName = rest['*'];
 
-  const [resolveContentFunction, setResolveContentFunction] = useState(() => () => fetchEvent(eventId));
-
-  const handleRsvp = useCallback(() => {
-    setResolveContentFunction(() => () => fetchEvent(eventId));
-  }, [
-    setResolveContentFunction,
-    eventId
-  ]); // TODO Better way to trigger rerender? :/
-
   const handleDelete = useCallback(() => {
     navigate!('/');
-  }, [
-    navigate
-  ]);
+  }, [navigate]);
 
   return (
     <section id="event-page">
       <LoadableContent
-        resolveContent={resolveContentFunction}
+        resolveContent={() => fetchEvent(eventId)}
         renderLoading={() => (
           <>
             <h1>{tmpEventName}</h1>
@@ -49,7 +38,7 @@ function EventPage({ eventId, navigate, ...rest }: EventPageProps) {
             />
           </>
         )}
-        renderSuccess={(event) => {
+        renderSuccess={(event, refreshEvent) => {
           const {
             name,
             description,
@@ -73,7 +62,7 @@ function EventPage({ eventId, navigate, ...rest }: EventPageProps) {
                     location={location}
                     owner={owner}
                   />
-                  <RsvpButton event={event} onRsvp={handleRsvp} />
+                  <RsvpButton event={event} onRsvp={refreshEvent} />
                   <DeleteButton event={event} onDelete={handleDelete} />
                 </HighlightedBox>
               </header>
