5fde9e64a9cd8cdf3599711d40ed36b4c2db6b93
Tin Anh Nguyen <anhtin95@gmail.com>
2021-01-24 22:44:11 +0100

    style(fonts): move @font-face to head in index.html
    Not really sure why, but the fonts will flicker otherwise 🤷. A
    side-effect of this change might increase the time to load the app,
    however, I did not notice any difference when testing the load time.

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -5,24 +5,6 @@ import { usingColors, usingStyling } from './hooks/theme';
 export const UBIQUITOUS_ANIMATION = 'fade-in 0.3s ease-in';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'ApexNew';
-    font-style: 'normal';
-    font-weight: 400;
-    src:
-      local('ApexNew-Book'),
-      url('/fonts/ApexNew-Book.otf') format('opentype');
-  }
-
-  @font-face {
-    font-family: 'ApexNew-Bold';
-    font-style: 'normal';
-    font-weight: 700;
-    src:
-      local('ApexNew-Bold'),
-      url('/fonts/ApexNew-Bold.otf') format('opentype');
-  }
-
   body {
     margin: 0;
     color: ${usingColors((c) => c.ON.PRIMARY)};
