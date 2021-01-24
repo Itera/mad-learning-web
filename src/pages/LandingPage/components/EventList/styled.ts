d9184f696b684121bca164ef39c86f9f8d11fa15
Tin Anh Nguyen <anhtin95@gmail.com>
2021-01-24 01:33:01 +0100

    bugfix(#42): grid and card width in Landing page
    Previously, the grid didn't really behave as a grid and the cards would
    be huge on a 4k monitor. Both of these issues are fixed by this commit
    by using CSS grid instead of flexbox and by setting a max-width,
    respectively.
    
    Note that the change might break the app on IE and older browsers,
    however, it should be fine as this app is only meant for internal usage.
    
    The motivation for changing from flexbox to CSS grid is because the
    latter is better at handling grid layouts. The only reason for why CSS
    grid was not used initially was due to an outdated assumption that it
    does not have great browser support - which is no longer true according
    to https://caniuse.com/?search=css-grid.

--- src/pages/LandingPage/components/EventList/styled.ts
+++ src/pages/LandingPage/components/EventList/styled.ts
@@ -2,27 +2,25 @@ import styled from 'styled-components';
 
 import { usingTypography } from 'src/hooks/theme';
 
+const MAX_NUM_COL = 4;
+const MAX_COL_WIDTH = 350;
+const HOR_MARGIN_LEVEL = 7;
+
 export const GridList = styled.ul`
-  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(${MAX_COL_WIDTH}px, 1fr));
+  grid-gap: ${usingTypography((t) => t.scaleSpacing(5))}px;
+  margin: auto;
   padding: 0;
-  text-align: center;
+  max-width: ${usingTypography((t) => {
+    const horMargins = t.scaleSpacing(HOR_MARGIN_LEVEL) * 2;
+    const colWidth = horMargins + MAX_COL_WIDTH;
+    return colWidth * MAX_NUM_COL;
+  })}px;
+  list-style-type: none;
 `;
 
-export const GridItem = styled.li`
-  display: inline-block;
-  width: 95vw;
-  margin-bottom: ${usingTypography((t) => t.scaleSpacing(7))}px;
-  text-align: start;
-
-  @media screen and (min-width: 620px) {
-    width: 44vw;
-    margin: 0 1.25vw 2.5vw;
-  }
-
-  @media screen and (min-width: 1250px) {
-    width: 28vw;
-  }
-`;
+export const GridItem = styled.li``;
 
 export const EventInfoList = styled.ul`
   list-style-type: none;
