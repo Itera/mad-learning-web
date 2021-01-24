3223e5a1b46c2f425d941f0452facbc964cad05e
Tin Anh Nguyen <anhtin95@gmail.com>
2021-01-24 23:23:32 +0100

    bugfix(routing): add scroll to top hack
    Due to accessibility reasons, reach-router will focus on the destination
    page's content after a navigation. However, a side-effect of the focus
    operation is to scroll to the focused content, which is an unexpected
    behavior for users relying on visuals. To fix this issue, without
    breaking the accessibility functionality, we must manually scroll to the
    top of the page after each navigation.
    
    For more details, see:
    - https://github.com/reach/router/issues/242; and
    - https://stackoverflow.com/a/56996986.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { Router } from '@reach/router';
 
 import AppWrapper from './components/AppWrapper';
@@ -17,14 +17,33 @@ function App() {
       <AppWrapper>
         <main>
           <Router>
-            {routes.map(({ path, component: Component }) => (
-              <Component key={path} path={path} />
-            ))}
+            <ScrollToTopHack path="/">
+              {routes.map(({ path, component: Component }) => (
+                <Component key={path} path={path} />
+              ))}
+            </ScrollToTopHack>
           </Router>
         </main>
       </AppWrapper>
     </Providers>
   );
+}
+
+/*
+ * Due to accessibility reasons, reach-router will focus on the destination
+ * page's content after a navigation. However, a side-effect of the focus
+ * operation is to scroll to the focused content, which is an unexpected
+ * behavior for users relying on visuals. To fix this issue, without breaking
+ * the accessibility functionality, we must manually scroll to the top of the
+ * page after each navigation.
+ *
+ * For more details, see:
+ * - https://github.com/reach/router/issues/242; and
+ * - https://stackoverflow.com/a/56996986.
+ */
+function ScrollToTopHack({ location, children }: any) {
+  useLayoutEffect(() => window.scrollTo(0, 0), [location.pathname]);
+  return children;
 }
 
 export default App;
