d164023780a7a3ea4c34330ca43a479ad2685b7f
erlenste <erlenste@ifi.uio.no>
2021-09-15 09:53:44 +0200

    add validation for image url link. add validation for location string length. add validation for start time exceeds end time and end time is before start time.

--- src/components/NewEventForm/NewEventForm.tsx
+++ src/components/NewEventForm/NewEventForm.tsx
@@ -64,36 +64,51 @@ function NewEventForm({ onSubmit, ...rest }: EventFormProps) {
 
   // Validation
   const EventFormSchema = Yup.object().shape({
-    name: 
-      Yup.string()
-         .max(100, "Event name is too long")
-         .required("Event name is required"),
-    eventType: 
-      Yup.string()
-         .oneOf(EVENT_OPTIONS)
-         .required('Event type is required'),
-    date: 
-      Yup.date()
-         .default(() => now)
-         .required('Event date is required'),
-    startTime: 
-      Yup.string()
-         .required("Start time is required"),
-    endTime: 
-      Yup.string()
-         .required("End time is required"),
-    description: 
-      Yup.string()
-         .max(5000, "Description text is too long")
-         .required('Description of event is required'),
-    eventStatus: 
-      Yup.string()
-         .oneOf([EVENT_STATUS_OPTIONS.Draft, EVENT_STATUS_OPTIONS.Published])
-         .required('Event status is required')
+    name: Yup
+      .string()
+      .max(100, "Event name is too long")
+      .required("Event name is required"),
+    eventType: Yup
+      .string()
+      .oneOf(EVENT_OPTIONS)
+      .required('Event type is required'),
+    date: Yup
+      .date()
+      .required('Event date is required'),
+    startTime: Yup
+      .string()
+      .required("Start time is required")
+      .test("Start Time", "Start time cannot be before end time", function(startTime) {
+        const startTimeHour = parseInt(this.parent.startTime.toString().substring(0,2));
+        const endTimeHour = parseInt(this.parent.endTime.toString().substring(0,2));
+        return startTimeHour < endTimeHour;
+      }),
+    endTime: Yup
+      .string()
+      .required("End time is required")
+      .test("End Time", "End time cannot be before start time", function(endTime) {
+        const startTimeHour = parseInt(this.parent.startTime.toString().substring(0,2));
+        const endTimeHour = parseInt(this.parent.endTime.toString().substring(0,2));
+        return endTimeHour > startTimeHour;
+      }),
+    description: Yup
+      .string()
+      .max(5000, "Description text is too long")
+      .required('Description of event is required'),
+    location: Yup
+      .string()
+      .max(200, "Location text is too long"),
+    imageUrl: Yup
+      .string()
+      .url("URL not valid"),
+    eventStatus: Yup
+      .string()
+      .oneOf([EVENT_STATUS_OPTIONS.Draft, EVENT_STATUS_OPTIONS.Published])
+      .required('Event status is required')
   });
 
   return (
-    <>
+    <div>
       <Formik
         initialValues={{ 
           name: rest.name || "",
@@ -178,7 +193,7 @@ function NewEventForm({ onSubmit, ...rest }: EventFormProps) {
             />
             <FormikInputField
               label = "Image Link"
-              name = "image"
+              name = "imageUrl"
               type = "text"
             />
             <FormikInputField
@@ -192,7 +207,7 @@ function NewEventForm({ onSubmit, ...rest }: EventFormProps) {
           </Form>
         </EventFormWrapper>
       </Formik>
-    </>
+    </div>
   );
 }
 
