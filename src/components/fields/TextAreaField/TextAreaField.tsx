fef08cc400aa38e4a9be59cc391881670ef5caf2
sebastto <sebast2106@gmail.com>
2021-09-15 17:47:07 +0200

    update TextAreaField component to support refs

--- src/components/fields/TextAreaField/TextAreaField.tsx
+++ src/components/fields/TextAreaField/TextAreaField.tsx
@@ -13,27 +13,23 @@ type TextAreaFieldProps = {
   onChange: Function;
 };
 
-function TextAreaField({
-  name,
-  value,
-  label,
-  placeholder,
-  resize = true,
-  onChange,
-}: TextAreaFieldProps) {
-  return (
-    <FieldWrapper>
-      {label && <FieldLabel htmlFor={name}>{label}</FieldLabel>}
-      <TextArea
-        id={name}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        resize={resize}
-        onChange={(e) => onChange(e.target.value)}
-      />
-    </FieldWrapper>
-  );
-}
+const TextAreaField = React.forwardRef<HTMLTextAreaElement, TextAreaFieldProps>(
+  ({ name, value, label, placeholder, resize = true, onChange }, ref) => {
+    return (
+      <FieldWrapper>
+        {label && <FieldLabel htmlFor={name}>{label}</FieldLabel>}
+        <TextArea
+          id={name}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          resize={resize}
+          onChange={(e) => onChange(e.target.value)}
+          ref={ref}
+        />
+      </FieldWrapper>
+    );
+  }
+);
 
 export default TextAreaField;
