e42e1d4b879884e8489ea28d4d3868517da2e66c
sebastto <sebast2106@gmail.com>
2021-09-29 14:13:00 +0200

    Add hero component

---
+++ src/components/Hero/styled.ts
@@ -0,0 +1,25 @@
+import { usingTypography, usingStyling } from 'src/hooks/theme';
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-left: -2.5vw;
+  margin-right: -2.5vw;
+  padding: 2.5vw;
+
+  height: 30vh;
+
+  background-color: lightgray;
+`;
+
+export const Header = styled.h1`
+  width: 70%;
+
+  font-size: ${usingTypography((t) => t.scaleFont(10))}px;
+  font-family: 'IBMPlexSans-Bold';
+
+  text-align: center;
+`;
