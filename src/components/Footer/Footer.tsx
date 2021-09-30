b1d4b84d614713c649f3f0d1e2ad830f69022c05
Erlend Stenlund <erlend.stenlund@itera.com>
2021-09-30 15:37:29 +0200

    update contact person

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,8 +8,7 @@ function Footer(props : any) {
     <PageFooter>
       <FooterTitle>{pageText}</FooterTitle>
       <PersonContaier>  
-        <ContactPerson image="https://www.iconninja.com/files/592/204/968/communication-male-cyber-avatar-man-person-crime-human-profile-head-anonymous-hacker-creative-icon.svg" name="Erlend Stenlund" email="erlend.stenlund@itera.com"/>
-        <ContactPerson image="https://www.iconninja.com/files/592/204/968/communication-male-cyber-avatar-man-person-crime-human-profile-head-anonymous-hacker-creative-icon.svg" name="Test Testy" email="test.testy@itera.com"/>
+        <ContactPerson image='https://as1.ftcdn.net/v2/jpg/01/26/91/78/1000_F_126917812_XlWgkaV9f81Hde4wvmvJWM3huJRvy5EM.jpg' name='Lars Erik Røyse' email='leroise@itera.com'/>
       </PersonContaier>
     </PageFooter>
   )
