b36b5591d38a7fffc97a13adf1158ccc296df2d9
Erlend Stenlund <erlend.stenlund@itera.com>
2021-10-05 12:59:30 +0200

    add HeroImage Component for Practises page

---
+++ src/components/HeroImage/styled.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const WideWrapper = styled.section`
+  margin: 2.5vw;
+  margin-left: -2.5vw;
+  margin-right: -2.5vw;
+`;
+
+export const CenteredWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MainImage = styled.img`
+  position: relative;
+  width: 100%;
+  height: 30vw;
+`;
+
+export const MainText = styled.h1`
+  position: absolute;
+  color: white;
+  font-family: 'IBMPlexSans';
+  font-size: 70px;
+`;
