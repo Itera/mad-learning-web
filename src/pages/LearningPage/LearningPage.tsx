151f5544c5ff9cd4b3bd1b61f86dbe77facd544d
Erlend Stenlund <erlend.stenlund@itera.com>
2021-10-08 10:42:06 +0200

    create Learning Page with route: /learning

--- src/pages/LearningPage/LearningPage.tsx
+++ src/pages/LearningPage/LearningPage.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import CutoutImageContainer from 'src/components/CutoutImageContainer';
 import Footer from 'src/components/Footer';
+import HeroImage from 'src/components/HeroImage';
 import { CutoffImageContainerWrapper } from '../LandingPage/styled';
-import { LearningPageContainer } from './styled';
+import { InfoText, InfoTextWrapper, LearningPageContainer } from './styled';
 
 function LearningPage() {
   return (
     <LearningPageContainer>
+      <HeroImage
+        imageSrc="https://fotophono.no/wp-content/uploads/2016/04/oslopanorama.jpg"
+        heroTitle="Learning"
+      />
+
+      <InfoTextWrapper>
+        <InfoText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut lectus
+          arcu bibendum at varius. Orci eu lobortis elementum nibh tellus
+          molestie nunc non blandit. Pulvinar etiam non quam lacus suspendisse.
+          Non enim praesent elementum facilisis leo vel. Vitae congue mauris
+          rhoncus aenean vel elit scelerisque mauris pellentesque.
+        </InfoText>
+      </InfoTextWrapper>
+
       <CutoffImageContainerWrapper>
-        {' '}
         <CutoutImageContainer
           imageText={'Online courses'}
           imageSrc={
@@ -30,7 +46,7 @@ function LearningPage() {
           imageSrc={
             'https://data.europa.eu/sites/default/files/news/2020-25-3.jpg'
           }
-          cutoffMode={'WideTopLeft'}
+          cutoffMode={'WideTopRight'}
           navigate={''}
         />
         <CutoutImageContainer
@@ -42,16 +58,24 @@ function LearningPage() {
           navigate={''}
         />
         <CutoutImageContainer
-          imageText={'How to\'s'}
+          imageText={"How to's"}
           imageSrc={
             'https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg'
           }
           cutoffMode={'HighBottomLeft'}
           navigate={''}
         />
+        <CutoutImageContainer
+          imageText={'Itera Dictionary'}
+          imageSrc={
+            'https://img.freepik.com/free-photo/closeup-beautiful-view-nature-green-leaves-blurred-greenery-tree-background-with-sunlight-public-garden-park-it-is-landscape-ecology-copy-space-wallpaper-backdrop_50039-1273.jpg?size=626&ext=jpg'
+          }
+          cutoffMode={'HighTopLeft'}
+          navigate={''}
+        />
       </CutoffImageContainerWrapper>
 
-      <Footer pageText='If you have any questions or inquieries, please contact'/>
+      <Footer pageText="If you have any questions or inquieries, please contact" />
     </LearningPageContainer>
   );
 }
